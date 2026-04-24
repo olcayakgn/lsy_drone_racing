13eafa3d344276676f16ed5c40130c9e727cef1b
TheoSpringer <theospringer2004@gmail.com>
2026-04-24 15:41:01 +0200

    Test

--- lsy_drone_racing/control/simple_racing_controller.py
+++ lsy_drone_racing/control/simple_racing_controller.py
@@ -11,7 +11,7 @@ Track layout (nominal, from config):
   Start: [-1.5, 0.75, 0.01] 
   Gate opening: 0.4m x 0.4m, outer frame 0.72m x 0.72m
   Obstacle poles: radius 0.015m, ~1.6m tall  
-"""
+""" 
 
 from __future__ import annotations
 
